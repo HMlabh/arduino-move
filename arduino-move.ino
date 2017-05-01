2f33a12f1d7e373bb36bae5dcb43b8529c1fde40
Metheus101 <gutekunstmarkus@gmx.net>
2017-05-01 11:56:15 +0200

    adj

--- arduino-move.ino
+++ arduino-move.ino
@@ -1,7 +1,7 @@
 /*
 *  Ansteuerung des Hubsystems
 *  Dies ist der Ansteuerungscode für die Schrittmotoren, welche den Hub des Prototyps übernehmen.
-*  Version 1.1
+*  Version 1.1b
 *  Stand: November 2016
 *  Dieser Code wurde im Rahmen einer Bachelorarbeit an der Hochschule München erstellt.
 *  Verfasser: Kevin Wayne Wallace
